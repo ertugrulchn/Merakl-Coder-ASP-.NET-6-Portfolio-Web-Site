443f651d695183c007e064c3279e6bf37f6be224
ertugrulchn <cihanertugrulemre368@gmail.com>
2022-06-26 02:12:38 +0300

    Writer Profile Controller Added

---
+++ UdemyProject/Areas/Writer/Controllers/ProfileController.cs
@@ -0,0 +1,53 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UdemyProject.Areas.Writer.Models;
+
+namespace UdemyProject.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public ProfileController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditViewModel model = new UserEditViewModel();
+            model.Name = values.Name;
+            model.Surname = values.Surname;
+            model.PictureURL = values.ImageUrl;
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel editViewModel)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (editViewModel.Picture != null)
+            {
+                var resource = Directory.GetCurrentDirectory();
+                var extension = Path.GetExtension(editViewModel.Picture.FileName);
+                var imageName = Guid.NewGuid() + extension;
+                var saveLocation = resource + "/wwwroot/UserImage/" + imageName;
+                var stream = new FileStream(saveLocation, FileMode.Create);
+                await editViewModel.Picture.CopyToAsync(stream);
+                user.ImageUrl = imageName;
+            }
+            user.Name = editViewModel.Name;
+            user.Surname = editViewModel.Surname;
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+            return View();
+        }
+    }
+}
